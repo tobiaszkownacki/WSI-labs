02012e1007a79ca6800ce52bcb75ab5009d1db59
Tobiasz Kownacki <tobiasz.kownacki@gmail.com>
2025-01-12 12:13:21 +0100

    chyba kodzik done

--- lab6/main.py
+++ lab6/main.py
@@ -3,10 +3,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 from typing import Callable
 
+LEARNING_RATE = 0.25
+DISCOUNT_FACTOR = 0.9
+
 
 class QLearnFrozenLake:
     DELTA_EPSILON = 0.00001
-    MIN_EPSILON = 0.1
+    MIN_EPSILON = 0.001
 
     def __init__(
         self,
@@ -45,18 +48,17 @@ class QLearnFrozenLake:
                 action = self.choose_action(
                     current_state, epsilon, qtable
                 )
-
                 next_state, reward, done, truncated, _ = self.env.step(action)
 
                 reward = reward_function(
                     reward, next_state, current_state, done
                 )
-
                 delta = (
                     reward
                     + self.discount_factor * np.max(qtable[next_state, :])
                     - qtable[current_state, action]
                 )
+
                 qtable[current_state, action] += self.learning_rate * delta
                 current_state = next_state
 
@@ -92,11 +94,11 @@ class QLearnFrozenLake:
         reward: int, next_state: int, current_state: int, done: bool
     ) -> int:
         # If the agent falls into a hole, the reward is -1
-        # if the agent reaches the goal, the reward is 100
+        # if the agent reaches the goal, the reward is 1
         if done and reward == 0:
             return -1
         elif done and reward == 1:
-            return 100
+            return 10
         return 0
 
     @staticmethod
@@ -104,71 +106,34 @@ class QLearnFrozenLake:
         reward: int, next_state: int, current_state: int, done: bool
     ) -> int:
         # If the agent falls into a hole, the reward is -5
-        # if the agent reaches the goal, the reward is 100
-        # if the agent hits the wall, the reward is -1
+        # if the agent reaches the goal, the reward is 20
+        # if the agent stays in the same state, the reward is -1
         if done and reward == 0:
             return -5
         elif done and reward == 1:
-            return 100
+            return 20
         elif next_state == current_state:
             return -1
         return 0
 
-    def evaluate_qtable(
-            self, num_of_episodes: int, num_of_steps: int,
-            ) -> int:
-        num_of_successes = 0
-        for _ in range(num_of_episodes):
-            current_state, _ = self.env.reset()
-            for _ in range(num_of_steps):
-                action = np.argmax(self.q_table[current_state, :])
-                next_state, reward, done, truncated, _ = self.env.step(action)
-                current_state = next_state
-                if done or truncated:
-                    break
-            if reward == 1:
-                num_of_successes += 1
-        success_rate = num_of_successes / num_of_episodes * 100
-        print(f"Success rate: {round(success_rate, 2)}%")
-        return success_rate
-
 
 def compare_average_rewards(
-        num_of_episodes: int,
-        num_of_steps: int,
+        episodes: int,
+        steps: int,
         num_of_id_runs: int,
         reward_function: Callable[[int, int, int, bool], int],
         is_slippery: bool = False
         ) -> np.array:
-    average_rewards = np.zeros(num_of_episodes)
+    average_rewards = np.zeros(episodes)
     for _ in range(num_of_id_runs):
         qlearn = QLearnFrozenLake(
-            num_of_episodes, num_of_steps, 0.1, 0.8, is_slippery
+            episodes, steps, LEARNING_RATE, DISCOUNT_FACTOR, is_slippery
             )
         qlearn.train(reward_function, average_rewards)
     return average_rewards / num_of_id_runs
 
 
-def compare_success_rates(
-        num_of_episodes: int,
-        num_of_steps: int,
-        num_of_id_runs: int,
-        reward_function: Callable[[int, int, int, bool], int],
-        is_slippery: bool = False
-        ) -> np.array:
-    success_rates = np.zeros(num_of_id_runs)
-    for i in range(num_of_id_runs):
-        qlearn = QLearnFrozenLake(
-            num_of_episodes, num_of_steps, 0.1, 0.8, is_slippery
-            )
-        print(f'{qlearn.q_table}')
-        qlearn.train(reward_function)
-        print(f'{qlearn.q_table}')
-        success_rates[i] = qlearn.evaluate_qtable(1000, 200)
-    return sum(success_rates) / num_of_id_runs
-
-
-def plot_results(averaged_reward_base: np.array, averaged_reward: np.array):
+def plot_results(averaged_reward_base: np.array, averaged_reward: np.array, max_episodes: int = 1000) -> None:
     fig = plt.figure()
     ax = fig.add_subplot(1, 1, 1)
     ax.spines['left'].set_position('center')
@@ -177,45 +142,40 @@ def plot_results(averaged_reward_base: np.array, averaged_reward: np.array):
     ax.spines['top'].set_color('none')
     ax.xaxis.set_ticks_position('bottom')
     ax.yaxis.set_ticks_position('left')
-    plt.plot(averaged_reward_base, 'r')
-    plt.plot(averaged_reward, 'b')
-    plt.show()
+    averaged_reward_base = np.convolve(
+        averaged_reward_base, np.ones(50) / 50, 'valid'
+    )
+    averaged_reward = np.convolve(
+        averaged_reward, np.ones(50) / 50, 'valid'
+    )
+    plt.xlim(0, max_episodes)
+    plt.plot(averaged_reward_base, 'r', label='Default reward function')
+    plt.plot(averaged_reward, 'b', label='Alternative reward function')
+    plt.legend(loc="upper left", fontsize=8)
+    plt.savefig("results.png")
 
 
 def main():
     num_of_id_runs = 25
-    num_of_episodes = 5000
+    num_of_episodes = 1000
     num_of_steps = 200
 
-    # averaged_reward_base = compare_average_rewards(
-    #     num_of_episodes,
-    #     num_of_steps,
-    #     num_of_id_runs,
-    #     QLearnFrozenLake.reward_default,
-    #     True
-    #     )
-    # averaged_reward = compare_average_rewards(
-    #     num_of_episodes,
-    #     num_of_steps,
-    #     num_of_id_runs,
-    #     QLearnFrozenLake.reward_alternative_1,
-    #     True
-    #     )
-
-    # plot_results(averaged_reward_base, averaged_reward)
-
-    success_rate = compare_success_rates(
+    averaged_reward_base = compare_average_rewards(
         num_of_episodes,
         num_of_steps,
         num_of_id_runs,
         QLearnFrozenLake.reward_default,
         True
         )
+    averaged_reward = compare_average_rewards(
+        num_of_episodes,
+        num_of_steps,
+        num_of_id_runs,
+        QLearnFrozenLake.reward_alternative_2,
+        True
+        )
 
-    print(
-        f"QTable trained on {num_of_episodes} episodes, "
-        f"Success rate: {success_rate}"
-    )
+    plot_results(averaged_reward_base, averaged_reward, num_of_episodes)
 
 
 if __name__ == "__main__":
